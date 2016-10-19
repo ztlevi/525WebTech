c06e68b56fd04cd51ffaf14bfd3e486e92a70ef0
ztlevi <zhouting@umich.edu>
2016-10-19 14:51:35 -0400

    implement all functions of Game of 15

--- GameOf15/game_of_15.js
+++ GameOf15/game_of_15.js
@@ -2,8 +2,12 @@ var $ = function(id) {
     return document.getElementById(id);
 };
 
+// set paramaters
 // pick one item in the array to be empty
 var numOfEmptyItem = Math.floor(Math.random() * 16);
+var numOfMoves = 0;
+var d = new Date();
+var startTime = d.getTime();
 
 function generateRandomTable() {
     var array = new Array();
@@ -24,6 +28,25 @@ function generateRandomTable() {
     }
 }
 
+var generateSimpleGame = function() {
+    for (var i = 0; i < 15; ++i) {
+        $("cell" + i).firstChild.nodeValue = i + 1;
+    }
+    $("cell" + 15).firstChild.nodeValue = null;
+    numOfEmptyItem = 15;
+    if (Math.random < 0.5) {
+        $("cell" + numOfEmptyItem).firstChild.nodeValue =
+            $("cell" + 14).firstChild.nodeValue;
+        $("cell" + 14).firstChild.nodeValue = null;
+        numOfEmptyItem = 14;
+    } else {
+        $("cell" + numOfEmptyItem).firstChild.nodeValue =
+            $("cell" + 11).firstChild.nodeValue;
+        $("cell" + 11).firstChild.nodeValue = null;
+        numOfEmptyItem = 11;
+    }
+}
+
 function swapToEmptyItem(numOfArray) {
     // click one item near the empty item, and swap it to the empty item
     if ((numOfArray - 1) == numOfEmptyItem || (numOfArray + 1) == numOfEmptyItem ||
@@ -32,18 +55,86 @@ function swapToEmptyItem(numOfArray) {
             $("cell" + numOfArray).firstChild.nodeValue;
         $("cell" + numOfArray).firstChild.nodeValue = null;
         numOfEmptyItem = numOfArray;
+
+        // calculate the Moves
+        numOfMoves++;
+        $("currentMoves").innerHTML = "Current Moves: " + numOfMoves + "<br>";
+
+        // calculate the used time
+        var d = new Date();
+        var used_time = d.getTime() - startTime;
+        var used_time = new Date(used_time);
+        $("usedTime").innerHTML = "Used Time: " + used_time.getMinutes() + "min " + used_time.getSeconds() + "s <br>";
+
+        // if game clear, then prumpt the confirm window and ask user if he
+        // wants to play again.
+        if (endOfGame()) {
+            if (confirm("Game clear! Do you want to play again?")) {
+                start();
+            } else {
+                window.close();
+            }
+        }
     }
 };
 
+var endOfGame = function() {
+    for (var i = 0; i < 15; ++i) {
+        if ($("cell" + i).firstChild.nodeValue != (i + 1)) {
+            return 0;
+            break;
+        }
+    }
+    return 1;
+};
+
 var start = function() {
+    // set paramaters
+    // pick one item in the array to be empty
+    numOfEmptyItem = Math.floor(Math.random() * 16);
+    numOfMoves = 0;
+    var d = new Date();
+    startTime = d.getTime();
+    $("currentMoves").innerHTML = "Current Moves: " + numOfMoves + "<br>";
+    $("usedTime").innerHTML = "Used Time: " + "0min " + "0s <br>";
+
+    // generate Table
     generateRandomTable();
+    $("newGame").addEventListener("click", start);
+    $("simpleGame").addEventListener("click", simple_game);
     for (var i = 0; i < 16; ++i) {
-        // there needs an function to pass i to num, so that it can be used
-        (function(num) {
-            $("cell" + i).addEventListener("click", function() {
+        // there needs an function to pass i to num
+        $("cell" + i).addEventListener("click", (function(num) {
+            return function() {
                 swapToEmptyItem(num);
-            })
-        })(i);
+            }
+        })(i));
     }
 };
+
+var simple_game = function() {
+    // set paramaters
+    // pick one item in the array to be empty
+    numOfEmptyItem = Math.floor(Math.random() * 16);
+    numOfMoves = 0;
+    var d = new Date();
+    startTime = d.getTime();
+    $("currentMoves").innerHTML = "Current Moves: " + numOfMoves + "<br>";
+    $("usedTime").innerHTML = "Used Time: " + "0min " + "0s <br>";
+
+    // generate Table
+    generateSimpleGame();
+    $("newGame").addEventListener("click", start);
+    $("simpleGame").addEventListener("click", simple_game);
+    for (var i = 0; i < 16; ++i) {
+        // there needs an function to pass i to num
+        $("cell" + i).addEventListener("click", (function(num) {
+            return function() {
+                swapToEmptyItem(num);
+            }
+        })(i));
+    }
+
+}
+
 window.addEventListener("load", start);
